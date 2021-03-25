9bad7d2b92aa895623b47b32fddaa2ca287eaf0b
Narindra Balkissoon <narindrab4@gmail.com>
2021-03-24 20:18:56 -0400

    pushing to fix error with vm ignore commit

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function Header(){
     return(
-        <header>
-
+        <header className="relative">
+            <div className="flex flex-row">
+                <i>Icon</i>
+                <div className="flex flex-col">
+                    <span>My Unsplash</span>
+                    <span>devchallenges.io</span>
+                </div>
+            </div>
+            <button id="add-photo" className="bg-green-500 rounded-md absolute top-0 right-0">Add a photo</button>
         </header>
     )
 }
