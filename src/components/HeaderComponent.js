0cb38a59f2d7193dd221fb7f7948fb4007fccda8
Narindra Balkissoon <narindrab4@gmail.com>
2021-03-25 11:43:56 -0400

    completed modal starting backend

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -20,9 +20,9 @@ export default function Header(){
         setToggleModal(!toggled)
         console.log(toggled)
 
-        modalStyle = (!toggled) ? "color:red" : "";
+        modalStyle = (!toggled) ? document.body.style.backgroundColor = "rgba(0, 0, 0, 0.25)" : document.body.style.backgroundColor = "#fff";
 
-        document.body.style.backgroundColor = "red";
+        
         
     }
     
@@ -34,7 +34,7 @@ export default function Header(){
             <div className="flex flex-row items-center">
                 <img className="h-8" src={logo}/>
                 <button className="relative z-10 left-10"><SearchOutlinedIcon className="h-4 w-4 text-gray-300"><SearchOutlinedIcon/></SearchOutlinedIcon></button>
-                <input type="text" className="z-0 border-2 border-gray-300 bg-white h-12 pl-12 rounded-lg text-sm focus:outline-none" placeholder="Search by name"/>
+                <input type="text" className="z-0 border-2 border-gray-300 h-12 pl-12 rounded-lg text-sm focus:outline-none" placeholder="Search by name"/>
                 <button onClick={toggleModal} id="add-photo" className="bg-green-500 rounded-md absolute top-0 right-0 p-3">Add a photo</button>
             </div>
             <Modal isOpen={toggled}/>
--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -3,19 +3,19 @@ import React from 'react';
 export default function Modal(props){
     if(props.isOpen){
     return(
-        <div className="bg-white shadow p-4 rounded-lg max-w-screen-sm">
-            <p className="text-black bold p-4">Add a new photo</p>
-            <div className="flex flex-col pl-2">
-                <label className="text-xs text-gray-700 p-1">Label</label>
-                <input className="border-2 border-gray-300 bg-white h-12 pl-2 rounded-lg text-sm focus:outline-none p-1" type="text" placeholder="Fruitless Basket"/>
+        <div className="bg-white shadow rounded-lg max-w-screen-sm p-8 mx-auto my-32">
+            <p className="text-black font-medium text-2xl">Add a new photo</p>
+            <div className="flex flex-col">
+                <label className="text-xs text-gray-700 py-2">Label</label>
+                <input className="border-2 border-gray-300 bg-white h-12 rounded-lg text-xs focus:outline-none py-2" type="text" placeholder="Fruitless Basket"/>
             </div>
-            <div className="flex flex-col pl-2">
-                <label className="text-xs">Photo URL</label>
-                <input className="border-2 border-gray-300 bg-white h-12 pl-2 rounded-lg text-sm focus:outline-none" type="text" placeholder="https://unsplash.com/photos/8ZcYCX5hmQ8"/>
+            <div className="flex flex-col">
+                <label className="text-xs text-gray-700 py-2">Photo URL</label>
+                <input className="border-2 border-gray-300 bg-white h-12 rounded-lg text-xs focus:outline-none py-2" type="text" placeholder="https://unsplash.com/photos/8ZcYCX5hmQ8"/>
             </div>
-            <div className="flex justify-end pl-2">
-                <button>Cancel</button>
-                <button>Submit</button>
+            <div className="flex justify-end py-4">
+                <button className="text-base text-gray-400 font-medium pr-6">Cancel</button>
+                <button className="bg-green-500 text-white rounded-lg px-4 h-14 text-base font-bold">Submit</button>
             </div>
         </div>
     )
