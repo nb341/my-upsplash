d0d7f0f25414a95cdc593ddeda6836ed27e8bcb6
Narindra Balkissoon <narindrab4@gmail.com>
2021-04-03 14:56:37 -0400

    added static files stuff to settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -124,8 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'/staticfiles'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 CORS_ALLOW_HEADERS = [
